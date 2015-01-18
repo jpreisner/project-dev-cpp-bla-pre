182692ceb032d6f91ef8e685bababbb34b8f700e
guillaume.blanchard37@gmail.com <guillaume.blanchard37@gmail.com@7c44edf9-8094-b9ef-f7da-1bebf3f47bfb>
2015-01-18 13:36:56 +0000

    Premier Commit

---
+++ trunk/ProjetCpp/src/ProjetCpp.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : ProjetCpp.cpp
+// Author      : G
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Projet Jeu de Role" << endl; // prints wèwèwè Ma Geule
+	return 0;
+}
